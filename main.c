af3ab1fc7e85a17be391c146fde12b71426cddbd
mutabor <mutaborblack@gmail.com>
2024-04-15 22:02:22 +0300

    Mutually inverse matrices (6 task)

--- main.c
+++ main.c
@@ -2,43 +2,42 @@
 #include <windows.h>
 #include "C:\Users\mutab\CLionProjects\13_laba\libs\data_structures\matrix\matrix.h"
 
-bool isUnique(int *a, int n) {
-    for (size_t i = 0; i < n - 1; i++) {
-        for (size_t j = i + 1; j < n; j++) {
-            if (a[i] == a[j])
-                return false;
+matrix mulMatrices(matrix m1, matrix m2) {
+    matrix result = getMemMatrix(m1.nRows, m2.nCols);
+    for (int i = 0; i < result.nRows; i++) {
+        for (int j = 0; j < result.nCols; j++) {
+            result.values[i][j] = 0;
+            for (size_t k = 0; k < m1.nCols; k++) {
+                result.values[i][j] += m1.values[i][k] * m2.values[k][j];
+            }
         }
     }
-    return true;
+    return result;
 }
 
-int getSum(int *a, int n) {
-    int sum = 0;
-    for (int i = 0; i < n; i++)
-        sum += a[i];
-    return sum;
-}
-
-void transposeIfMatrixHasNotEqualSumOfRows(matrix m) {
-    long long sumArray[m.nRows];
-    for (int i = 0; i < m.nRows; i++) {
-        sumArray[i] = getSum(m.values[i], m.nCols);
-    }
-    if (isUnique(sumArray, m.nRows)) {
-        transposeSquareMatrix(&m);
-    }
+bool isMutuallyInverseMatrices(matrix m1, matrix m2) {
+    matrix m = mulMatrices(m1, m2);
+    return isEMatrix(&m) ? true : false;
 }
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
-    int quantity_rows, quantity_columns;
-    printf("Введите количество рядов и столбцов: ");
-    scanf("%d %d", &quantity_rows, &quantity_columns);
+    int quantity_rows_A, quantity_columns_A;
+    printf("Введите количество рядов и столбцов для матрицы A: ");
+    scanf("%d %d", &quantity_rows_A, &quantity_columns_A);
 
-    matrix m = getMemMatrix(quantity_rows, quantity_columns);
-    inputMatrix(&m);
-    transposeIfMatrixHasNotEqualSumOfRows(m);
-    printMatrix(m);
+    matrix A = getMemMatrix(quantity_rows_A, quantity_columns_A);
+    printf("Введите элементы матрицы: ");
+    inputMatrix(&A);
+
+    int quantity_rows_B, quantity_columns_B;
+    printf("Введите количество рядов и столбцов для матрицы B: ");
+    scanf("%d %d", &quantity_rows_B, &quantity_columns_B);
+
+    matrix B = getMemMatrix(quantity_rows_B, quantity_columns_B);
+    inputMatrix(&B);
+
+    printf("%d", isMutuallyInverseMatrices(A, B));
 
     return 0;
 }
