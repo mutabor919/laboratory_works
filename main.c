6bb354371cebef9a053208edfc437f094a842d98
mutabor <mutaborblack@gmail.com>
2024-04-15 21:22:22 +0300

    Replace with a square (4 task)

--- main.c
+++ main.c
@@ -2,18 +2,23 @@
 #include <windows.h>
 #include "C:\Users\mutab\CLionProjects\13_laba\libs\data_structures\matrix\matrix.h"
 
-int getMin(int *a, int n) {
-    int min = a[0];
-    for (int i = 1; i < n; i++) {
-        if (a[i] < min) {
-            min = a[i];
+matrix mulMatrices(matrix m1, matrix m2) {
+    matrix result = getMemMatrix(m1.nRows, m2.nCols);
+    for (int i = 0; i < result.nRows; i++) {
+        for (int j = 0; j < result.nCols; j++) {
+            result.values[i][j] = 0;
+            for (size_t k = 0; k < m1.nCols; k++) {
+                result.values[i][j] += m1.values[i][k] * m2.values[k][j];
+            }
         }
     }
-    return min;
+    return result;
 }
 
-void sortRowsByMinElement(matrix m) {
-    selectionSortColsMatrixByColCriteria(m, getMin);
+void getSquareOfMatrixIfSymmetric(matrix *m) {
+    if (isSymmetricMatrix(m)) {
+        *m = mulMatrices(*m, *m);
+    }
 }
 
 int main() {
@@ -23,8 +28,9 @@ int main() {
     scanf("%d %d", &quantity_rows, &quantity_columns);
 
     matrix m = getMemMatrix(quantity_rows, quantity_columns);
+    printf("Введите элементы матрицы: ");
     inputMatrix(&m);
-    sortRowsByMinElement(m);
+    getSquareOfMatrixIfSymmetric(&m);
     printMatrix(m);
 
     return 0;
