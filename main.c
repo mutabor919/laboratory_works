c6a1094875850ee721ea5cd494a01d9a047de54f
mutabor <mutaborblack@gmail.com>
2024-04-22 15:47:27 +0300

    Get sum of the maximum elements of all pseudodiagonals (7 task)

--- main.c
+++ main.c
@@ -2,42 +2,52 @@
 #include <windows.h>
 #include "C:\Users\mutab\CLionProjects\13_laba\libs\data_structures\matrix\matrix.h"
 
-matrix mulMatrices(matrix m1, matrix m2) {
-    matrix result = getMemMatrix(m1.nRows, m2.nCols);
-    for (int i = 0; i < result.nRows; i++) {
-        for (int j = 0; j < result.nCols; j++) {
-            result.values[i][j] = 0;
-            for (size_t k = 0; k < m1.nCols; k++) {
-                result.values[i][j] += m1.values[i][k] * m2.values[k][j];
+int getSum(int *a, int n) {
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += a[i];
+    }
+    return sum;
+}
+
+int max2(int a, int b) {
+    return a > b ? a : b;
+}
+
+long long findSumOfMaxesOfPseudoDiagonal(matrix m) {
+    int quantity = m.nRows + m.nCols - 2;
+    int max_pseudo_diagonal[quantity];
+
+    for (size_t i = 0; i < quantity; i++) {
+        max_pseudo_diagonal[i] = 0;
+    }
+
+    int i_pseudo_diagonal;
+    for (int i = 0; i < m.nRows; i++) {
+        for (int j = 0; j < m.nCols; j++) {
+            if (j != i) {
+                if (i > j) {
+                    i_pseudo_diagonal = j - i + m.nRows - 1;
+                } else {
+                    i_pseudo_diagonal = j - i + m.nRows - 2;
+                }
+                max_pseudo_diagonal[i_pseudo_diagonal] = max2(max_pseudo_diagonal[i_pseudo_diagonal], m.values[i][j]);
             }
         }
     }
-    return result;
-}
-
-bool isMutuallyInverseMatrices(matrix m1, matrix m2) {
-    matrix m = mulMatrices(m1, m2);
-    return isEMatrix(&m) ? true : false;
+    return getSum(max_pseudo_diagonal, quantity);
 }
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
-    int quantity_rows_A, quantity_columns_A;
-    printf("Введите количество рядов и столбцов для матрицы A: ");
-    scanf("%d %d", &quantity_rows_A, &quantity_columns_A);
+    int quantity_rows, quantity_columns;
+    printf("Введите количество рядов и столбцов: ");
+    scanf("%d %d", &quantity_rows, &quantity_columns);
 
-    matrix A = getMemMatrix(quantity_rows_A, quantity_columns_A);
+    matrix m = getMemMatrix(quantity_rows, quantity_columns);
     printf("Введите элементы матрицы: ");
-    inputMatrix(&A);
-
-    int quantity_rows_B, quantity_columns_B;
-    printf("Введите количество рядов и столбцов для матрицы B: ");
-    scanf("%d %d", &quantity_rows_B, &quantity_columns_B);
-
-    matrix B = getMemMatrix(quantity_rows_B, quantity_columns_B);
-    inputMatrix(&B);
-
-    printf("%d", isMutuallyInverseMatrices(A, B));
+    inputMatrix(&m);
+    printf("%lld", findSumOfMaxesOfPseudoDiagonal(m));
 
     return 0;
 }
