bc64be7416a70e8d35a2c57f9170f157fc2ef3f7
mutabor <mutaborblack@gmail.com>
2024-04-22 17:34:26 +0300

    number of unique sums of matrix rows (10 task)

--- main.c
+++ main.c
@@ -1,36 +1,49 @@
 #include <stdio.h>
 #include <windows.h>
-#include <math.h>
 #include "C:\Users\mutab\CLionProjects\13_laba\libs\data_structures\matrix\matrix.h"
 
-float getDistance(int *a, int n) {
-    float distance = 0;
-    for (int i = 0; i < n; i++) {
-        distance += a[i] * a[i];
+int cmp_long_long(const void *pa, const void *pb) {
+    long long a = *(const long long *) pa;
+    long long b = *(const long long *) pb;
+
+    if (a < b) {
+        return -1;
+    } else if (a > b) {
+        return 1;
     }
-    return sqrt(distance);
+    return 0;
 }
 
-void insertionSortRowsMatrixByRowCriteriaF(matrix m, float (*criteria)(int *, int)) {
-    float criteria_array[m.nRows];
-    for (int i = 0; i < m.nRows; i++) {
-        criteria_array[i] = criteria(m.values[i], m.nCols);
-    }
-
-    for (int i = 1; i < m.nRows; i++) {
-        float t = criteria_array[i];
-        int j = i;
-        while (j > 0 && criteria_array[j - 1] > t) {
-            criteria_array[j] = criteria_array[j - 1];
-            swapRows(m, j, j - 1);
-            j--;
+int countNUnique(long long *a, int n) {
+    qsort(a, n, sizeof(long long), cmp_long_long);
+    int count = 0;
+    int unique = 0;
+    for (int i = 1; i < n; i++) {
+        if (a[i] == a[i - 1]) {
+            count++;
         }
-        criteria_array[j] = t;
+        if (count > 0) {
+            unique++;
+            count = 0;
+        }
     }
+    return unique;
 }
 
-void sortByDistances(matrix m) {
-    insertionSortRowsMatrixByRowCriteriaF(m, getDistance);
+int getSum(int *a, int n) {
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += a[i];
+    }
+    return sum;
+}
+
+int countEqClassesByRowsSum(matrix m) {
+    long long sum_rows[m.nRows];
+    for (int i = 0; i < m.nCols; i++) {
+        sum_rows[i] = getSum(m.values[i], m.nCols);
+    }
+    return countNUnique(sum_rows, m.nRows);
 }
 
 int main() {
@@ -41,8 +54,7 @@ int main() {
 
     matrix m = getMemMatrix(quantity_rows, quantity_columns);
     inputMatrix(&m);
-    sortByDistances(m);
-    printMatrix(m);
+    printf("%d", countEqClassesByRowsSum(m));
 
     return 0;
 }
