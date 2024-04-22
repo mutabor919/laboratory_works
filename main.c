1cf85029857c530c626c242e8a7f22a0beee73e5
mutabor <mutaborblack@gmail.com>
2024-04-22 18:35:55 +0300

    Replace the penultimate row of the matrix (11 task)

--- main.c
+++ main.c
@@ -2,22 +2,33 @@
 #include <windows.h>
 #include "C:\Users\mutab\CLionProjects\13_laba\libs\data_structures\matrix\matrix.h"
 
-int getNSpecialElement(matrix m) {
-    int quantity_special_elements = 0;
-    for (int j = 0; j < m.nCols; ++j) {
-        for (int i = 0; i < m.nRows; ++i) {
-            int sum = 0;
-            for (int k = 0; k < m.nRows; ++k) {
-                if (k != i) {
-                    sum += m.values[k][j];
-                }
-            }
-            if (m.values[i][j] > sum) {
-                quantity_special_elements++;
+position getLeftMin(matrix m) {
+    int min = m.values[0][0];
+    int min_by_row = 0;
+    int min_by_column = 0;
+
+    for (int i = 0; i < m.nCols; i++) {
+        for (int j = 0; j < m.nRows; j++) {
+            if (m.values[j][i] < min) {
+                min = m.values[j][i];
+                min_by_row = j;
+                min_by_column = i;
             }
         }
     }
-    return quantity_special_elements;
+    return (position) {min_by_row, min_by_column};
+}
+
+void swapPenultimateRow(matrix m) {
+    position min_position = getLeftMin(m);
+    int min_element_column[m.nRows];
+    for (int i = 0; i < m.nRows; ++i) {
+        min_element_column[i] = m.values[i][min_position.colIndex];
+    }
+
+    for (size_t i = 0; i < m.nRows; i++) {
+        m.values[m.nRows - 2][i] = min_element_column[i];
+    }
 }
 
 int main() {
@@ -29,7 +40,8 @@ int main() {
     matrix m = getMemMatrix(quantity_rows, quantity_columns);
     printf("Введите элементы матрицы: ");
     inputMatrix(&m);
-    printf("%d", getNSpecialElement(m));
+    swapPenultimateRow(m);
+    printMatrix(m);
 
     return 0;
 }
