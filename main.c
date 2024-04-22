dda47622b2a0adf02749179c9b8774657ffefefd
mutabor <mutaborblack@gmail.com>
2024-04-22 18:02:31 +0300

    number of special matrix elements (11 task)

--- main.c
+++ main.c
@@ -2,48 +2,22 @@
 #include <windows.h>
 #include "C:\Users\mutab\CLionProjects\13_laba\libs\data_structures\matrix\matrix.h"
 
-int cmp_long_long(const void *pa, const void *pb) {
-    long long a = *(const long long *) pa;
-    long long b = *(const long long *) pb;
-
-    if (a < b) {
-        return -1;
-    } else if (a > b) {
-        return 1;
-    }
-    return 0;
-}
-
-int countNUnique(long long *a, int n) {
-    qsort(a, n, sizeof(long long), cmp_long_long);
-    int count = 0;
-    int unique = 0;
-    for (int i = 1; i < n; i++) {
-        if (a[i] == a[i - 1]) {
-            count++;
-        }
-        if (count > 0) {
-            unique++;
-            count = 0;
+int getNSpecialElement(matrix m) {
+    int quantity_special_elements = 0;
+    for (int j = 0; j < m.nCols; ++j) {
+        for (int i = 0; i < m.nRows; ++i) {
+            int sum = 0;
+            for (int k = 0; k < m.nRows; ++k) {
+                if (k != i) {
+                    sum += m.values[k][j];
+                }
+            }
+            if (m.values[i][j] > sum) {
+                quantity_special_elements++;
+            }
         }
     }
-    return unique;
-}
-
-int getSum(int *a, int n) {
-    int sum = 0;
-    for (int i = 0; i < n; i++) {
-        sum += a[i];
-    }
-    return sum;
-}
-
-int countEqClassesByRowsSum(matrix m) {
-    long long sum_rows[m.nRows];
-    for (int i = 0; i < m.nCols; i++) {
-        sum_rows[i] = getSum(m.values[i], m.nCols);
-    }
-    return countNUnique(sum_rows, m.nRows);
+    return quantity_special_elements;
 }
 
 int main() {
@@ -53,8 +27,9 @@ int main() {
     scanf("%d %d", &quantity_rows, &quantity_columns);
 
     matrix m = getMemMatrix(quantity_rows, quantity_columns);
+    printf("Введите элементы матрицы: ");
     inputMatrix(&m);
-    printf("%d", countEqClassesByRowsSum(m));
+    printf("%d", getNSpecialElement(m));
 
     return 0;
 }
