87aaa92516112c185b7174dcfafd1689d24b43a0
mutabor <mutaborblack@gmail.com>
2024-05-14 00:17:19 +0300

    Get a string in which the words of the first and second lines alternate (18 lab)

--- main.c
+++ main.c
@@ -1,37 +1,44 @@
 #include <stdio.h>
 #include "C:\Users\mutab\CLionProjects\13_laba\libs\str\string\string_.h"
 
-int countPalindromes(char *s) {
-    char *endSearch = getEndOfString(s);
-    char *beginSearch = findNonSpace(s);
-    int countPalindromes = 0;
-    char *commaPos = find(beginSearch, endSearch, ',');
-    bool isLastComma = *commaPos == '\0' && endSearch != beginSearch;
-
-    while (*commaPos != '\0' || isLastComma) {
-        beginSearch = findNonSpace(beginSearch);
-        countPalindromes += isPalindrome(beginSearch, commaPos);
-        beginSearch = commaPos + 1;
-        if (isLastComma)
-            break;
-        commaPos = find(beginSearch, endSearch, ',');
-        isLastComma = *commaPos == '\0';
+void combiningTwoStrings(char *s, char *s1, char *s2) {
+    char *beginCopy = s;
+    char *beginSearch1 = s1, *beginSearch2 = s2;
+    WordDescriptor word1, word2;
+    bool isW1Found = true, isW2Found;
+    while (isW1Found || isW2Found) {
+        isW1Found = getWord(beginSearch1, &word1);
+        isW2Found = getWord(beginSearch2, &word2);
+        if (isW1Found) {
+            beginCopy = copy(word1.begin, word1.end, beginCopy);
+            *beginCopy++ = ' ';
+            beginSearch1 = word1.end;
+        }
+        if (isW2Found) {
+            beginCopy = copy(word2.begin, word2.end, beginCopy);
+            *beginCopy++ = ' ';
+            beginSearch2 = word2.end;
+        }
     }
-    return countPalindromes;
+    *(beginCopy - 1) = '\0';
 }
 
-void test_countPalindromes() {
-    char s[] = "Test,for,laboratory,eighteen";
-    assert(countPalindromes(s) == 0);
+void test_combiningTwoStrings() {
+    char s1[] = "T e s t f o r";
+    char s2[] = "l a b o r a t o r y";
+    char str[15];
+    combiningTwoStrings(str, s1, s2);
+    ASSERT_STRING("T l e a s b t o f r o a r t o r y", str);
 
-    char s2[] = "";
-    assert(countPalindromes(s2) == 0);
-
-    char s3[] = "Test,ror";
-    assert(countPalindromes(s3) == 1);
+    char s3[] = "a b c";
+    char s4[] = "d e f";
+    char str2[10];
+    combiningTwoStrings(str2, s3, s4);
+    ASSERT_STRING("a d b e c f", str2);
 }
 
 int main() {
-    test_countPalindromes();
+    test_combiningTwoStrings();
+
     return 0;
 }
