f30e1e29af09da87ebfd915f58f8022d097f6856
mutabor <mutaborblack@gmail.com>
2024-05-13 18:13:11 +0300

    Convert a string leaving only one character (18 lab)

--- main.c
+++ main.c
@@ -1,42 +1,41 @@
 #include <stdio.h>
-#include <string.h>
 #include "C:\Users\mutab\CLionProjects\13_laba\libs\str\string\string_.h"
 
-int isGraph(char s) {
-    return !isspace(s);
+void removeAdjacentEqualLetters(char *s) {
+    if ((s == NULL || strlen(s) == 0))
+        return;
+    int i, j;
+    for (i = 0, j = 0; s[i] != '\0'; i++) {
+        if (s[i] != s[i + 1]) {
+            s[j] = s[i];
+            j++;
+        }
+    }
+    s[j] = '\0';
 }
 
-void removeNonLetters(char *s) {
-    char *endSource = getEndOfString(s);
-    char *destination = copyIf(s, endSource, s, isGraph);
-    *destination = '\0';
+void test_removeAdjacentEqualLetters_nonEqualLetters() {
+    char s[] = "Test";
+    removeAdjacentEqualLetters(s);
+    ASSERT_STRING("Test", s);
 }
 
-void test_removeNonLetters_nonWords() {
+void test_removeAdjacentEqualLetters_noWords() {
     char s[] = " ";
-    removeNonLetters(s);
-    ASSERT_STRING ("", s);
+    removeAdjacentEqualLetters(s);
+    ASSERT_STRING(" ", s);
 }
 
-void test_removeNonLetters_oneWord() {
-    char s[] = "abc";
-    removeNonLetters(s);
-    ASSERT_STRING ("abc", s);
-}
-
-void test_removeNonLetters_manyWords() {
-    char s[] = "abc abc";
-    removeNonLetters(s);
-    ASSERT_STRING ("abcabc", s);
-}
-
-void test_removeNonLetters() {
-    test_removeNonLetters_oneWord();
-    test_removeNonLetters_manyWords();
-    test_removeNonLetters_nonWords();
+void test_removeAdjacentEqualLetters_EqualLetters() {
+    char s[] = "TTTTeesssttttttffooooorr";
+    removeAdjacentEqualLetters(s);
+    ASSERT_STRING("Testfor", s);
 }
 
 int main() {
-    test_removeNonLetters();
+    test_removeAdjacentEqualLetters_noWords();
+    test_removeAdjacentEqualLetters_nonEqualLetters();
+    test_removeAdjacentEqualLetters_EqualLetters();
+
     return 0;
 }
