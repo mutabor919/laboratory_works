e6a8b2c2ccb43df596f2187032b44f95584a83a9
mutabor <mutaborblack@gmail.com>
2024-05-14 15:02:51 +0300

    Print the word in the given line that precedes the first word containing the letter A (18 lab)

--- main.c
+++ main.c
@@ -1,22 +1,63 @@
 #include <stdio.h>
 #include "C:\Users\mutab\CLionProjects\13_laba\libs\str\string\string_.h"
 
-void reverseWords(char *s) {
-    BagOfWords bag;
-    getBagOfWords(&bag, s);
-    for (int i = bag.size - 1; i >= 0; i--) {
-        char *ptr = bag.words[i].begin;
-        while (ptr != bag.words[i].end) {
-            printf("%c", *ptr);
-            ptr++;
+typedef enum WordBeforeFirstWordWithAReturnCode {
+    FIRST_WORD_WITH_A,
+    NOT_FOUND_A_WORD_WITH_A,
+    WORD_FOUND,
+    EMPTY_STRING
+} WordBeforeFirstWordWithAReturnCode;
+
+WordBeforeFirstWordWithAReturnCode getWordBeforeFirstWordWithA(char *s, WordDescriptor *w) {
+    WordDescriptor word;
+    char *ptr = s;
+    char *prevWordEnd = NULL;
+    int foundAWord = 0;
+
+    while (getWord(&ptr, &word)) {
+        if (find(word.begin, word.end, 'a') != word.end) {
+            if (!foundAWord) {
+                *w = word;
+                return FIRST_WORD_WITH_A;
+            } else {
+                if (prevWordEnd != NULL) {
+                    w->begin = s;
+                    w->end = prevWordEnd;
+                    return WORD_FOUND;
+                } else {
+                    return EMPTY_STRING;
+                }
+            }
         }
-        printf(" ");
+
+        prevWordEnd = word.end;
+        foundAWord = 1;
     }
+
+    if (ptr == s) {
+        return EMPTY_STRING;
+    }
+
+    return NOT_FOUND_A_WORD_WITH_A;
+}
+
+void test_getWordBeforeFirstWordWithA() {
+    WordDescriptor word;
+
+    char s1[] = "ab for test";
+    char s2[] = "Test for eighteen";
+    char s3[] = "";
+    char s4[] = "Test for aboratory";
+
+    assert(getWordBeforeFirstWordWithA(s1, &word) == FIRST_WORD_WITH_A &&
+           getWordBeforeFirstWordWithA(s2, &word) == NOT_FOUND_A_WORD_WITH_A &&
+           getWordBeforeFirstWordWithA(s3, &word) == EMPTY_STRING &&
+           getWordBeforeFirstWordWithA(s4, &word) == WORD_FOUND);
+
 }
 
 int main() {
-    char srt[] = "Test for laboratory eighteen";
-    reverseWords(srt);
+    test_getWordBeforeFirstWordWithA();
 
     return 0;
 }
